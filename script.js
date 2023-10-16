d403f1553cf974979de859a64a82c250151b8a23
Kamil MAZAN <104743332+KamilMAZAN@users.noreply.github.com>
2023-10-16 22:42:50 +0200

    ds

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleFullScreen() {
+    var image = document.querySelector('image-containerimg1 img');
+    if (!document.fullscreenElement) {
+      if (image.requestFullscreen) {
+        image.requestFullscreen();
+      } else if (image.mozRequestFullScreen) {
+        image.mozRequestFullScreen();
+      } else if (image.webkitRequestFullscreen) {
+        image.webkitRequestFullscreen();
+      } else if (image.msRequestFullscreen) {
+        image.msRequestFullscreen();
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    }
+  }
